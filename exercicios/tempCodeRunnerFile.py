2343fe4a3552c036b344414bb3c66b2eb6b52590
divinostnv <divinostnv@gmail.com>
2026-04-20 20:36:52 -0300

    Delete exercicios/tempCodeRunnerFile.py

--- exercicios/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
- parar=str(input('deseja continuar [S/N^] ? ')).upper()
-    while parar not in respostas:
-        parar=str(input('deseja continuar [S/N^] ? ')).upper()
-        if parar != 'S' and parar != 'N':
-            print('Resposta invalida, tente novamente. ')
