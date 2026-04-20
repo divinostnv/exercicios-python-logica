d3339ccad6b994106372c0bb4d0d7f93e21cab58
divinostnv <divinostnv@gmail.com>
2026-04-20 20:36:22 -0300

    Delete exercicios/beiço.py

--- exercicios/beiço.py
+++
@@ -1,8 +0,0 @@
-nome= str(input('Digite uma palavra: ')).lower()
-contv=0
-contc=0
-if nome in 'aeiou':
-    contv+=1
-else:
-    contc+=1
-print(f'A palavra {nome} tem {contc} consoante(s) e {contv} vogal(es).')
