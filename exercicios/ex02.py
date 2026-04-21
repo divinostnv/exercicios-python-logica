6c853913957010629bb35da5104c74b7e523d123
divinostnv <divinostnv@gmail.com>
2026-04-20 21:07:33 -0300

    Create ex02.py

---
+++ exercicios/ex02.py
@@ -0,0 +1,4 @@
+#faca um progama que leia o nome de uma pessoa e mostre uma mensagem de boas vindas.
+nome = input('Qual o seu nome: ')
+msg = 'É um prazer te conhecer'
+print(f'\033[34m{msg}, {nome}')
