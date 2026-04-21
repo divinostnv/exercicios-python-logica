8b094cf8eb7c772c454632e7d60dcac923c520b9
divinostnv <divinostnv@gmail.com>
2026-04-20 21:09:53 -0300

    Delete ex05_par_ou_impar.py

--- ex05_par_ou_impar.py
+++
@@ -1,5 +0,0 @@
-#faça um programa que leia um numero inteiro e mostre seu sucessor e antecessor.
-n= int (input('digite um numero: '))
-n1= n-1
-n2= n+1
-print('seu antecessor é {}, e seu sucessor é {}'.format(n1, n2))
