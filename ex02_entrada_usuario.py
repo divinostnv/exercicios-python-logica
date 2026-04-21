d5fd8619207dcd6cb1326c11eba202530d7c4369
divinostnv <divinostnv@gmail.com>
2026-04-20 21:09:26 -0300

    Delete ex02_entrada_usuario.py

--- ex02_entrada_usuario.py
+++
@@ -1,4 +0,0 @@
-#faca um progama que leia o nome de uma pessoa e mostre uma mensagem de boas vindas.
-nome = input('Qual o seu nome: ')
-msg = 'É um prazer te conhecer'
-print(f'\033[34m{msg}, {nome}')
